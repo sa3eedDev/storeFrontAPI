ad26ce0c2778d2d8318c2a6f942bd42d7c77990d
Sa3eedDev <36833212+sa3eedDev@users.noreply.github.com>
2022-11-14 22:58:14 -0500

    cahnged README.md

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createOrder, showOrder } from '../handlers/orders';
+import { verifyAuthToken } from '../middlewares/validetor';
 
 const ordersRoutes = (app: express.Application) =>{
     app.post('/orders', createOrder)
-    app.get("/orders/:id", showOrder)
+    app.get("/orders/:id",  verifyAuthToken,showOrder)
 }
 
 export default ordersRoutes;
